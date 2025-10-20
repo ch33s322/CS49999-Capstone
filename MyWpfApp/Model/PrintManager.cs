ffd16ed61eec1360e44c43d9d9a9e5933cdfe82f
Connor Blaha <blaha.s.connor@gmail.com>
2025-10-20 16:06:53 -0400

    added print manager class with undefined functions

---
+++ MyWpfApp/Model/PrintManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWpfApp.Model
+{
+    public class PrintManager
+    {
+        //associating printers to jobs
+        //associating jobs to printers
+        //set of all jobs
+        //set of all printers
+
+        //queues a job
+        public void QueueJob(Job job)
+        {
+
+        }
+
+        //release job
+        public void ReleaseJob(Guid jobId)
+        {
+
+        }
+
+        //set job to simplex/duplex
+        public void SetJobSimplexFlag(Guid jobId, bool flag)
+        {
+
+        }
+
+        //set job printer
+        public void SetJobPrinter(Guid jobId, string printerName)
+        {
+
+        }
+
+        //adds printer to printer set
+        public void AddPrinter()
+        {
+
+        }
+        //removes pritner from printer set and displaces all jobs to a special flag printer such that they are unassociated with a printer
+        public void RemovePrinter()
+        {
+
+        }
+
+        //used to get all jobs
+        public List<Job> GetJobs()
+        {
+            //will implement
+            return new List<Job>();
+        }
+
+        //get jobs for a printer, returns unsorted list of jobs for printer
+        public List<Job> GetJobsForPrinter(string printer)
+        {
+            //will implement
+            return new List<Job>();
+        }
+
+        //get all printers
+        public List<string> getAllPrinters()
+        {
+            //will implement
+            return new List<string>();
+        }
+    }
+}
